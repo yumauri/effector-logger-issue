82c5b776e28440b2ff9015ed4e4f91a6fde040a2
Victor Didenko <victord@setplex.com>
2022-06-27 19:37:33 +0300

    samples

---
+++ sample.mjs
@@ -0,0 +1,21 @@
+import {
+  sample,
+  createStore,
+  createEvent,
+} from "effector";
+import { debug } from "patronum/debug";
+
+const passingBy = createEvent({ name: "passingBy" });
+const goAhead = createEvent({ name: "goAhead" });
+const $gate = createStore(true, { name: "$gate" })
+  .on(passingBy, () => false)
+  .on(goAhead, () => true);
+
+sample({
+  clock: passingBy,
+  target: goAhead
+});
+
+debug(passingBy, goAhead, $gate);
+
+passingBy();
